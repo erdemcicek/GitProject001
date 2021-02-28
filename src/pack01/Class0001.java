dd654d7094738d4fad8f83b7b6a3a2aaea9932cd
erdemcicek <79759792+erdemcicek@users.noreply.github.com>
2021-02-28 17:41:05 +0100

    Update Class0001.java
    brand new

--- src/pack01/Class0001.java
+++ src/pack01/Class0001.java
@@ -6,10 +6,10 @@ public class Class0001 {
 		// Welcome to Sessions
 		// Thank you for joining
 		// Here is a new commit
-<<<<<<< HEAD
+
 		// Something different
-=======
+
 		// Here is new from remote
->>>>>>> 2e63eefabe2c83da709a3dbcc031e8dbf3546f56
+		// brand new
 	}
 }
